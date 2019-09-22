a66570d8cb1e75d0ff16c7f727312040974e62b4
Ricardo Varela <rvarela@deloitte.pt>
2019-09-22 18:38:22 +0100

    Add first method

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -1,5 +1,12 @@
 #!/usr/bin/env groovy
 
-def call(String name = 'human') {
-  echo "Hello, ${name}."
+def call(body) {
+    echo "Check status"
+
+    (1..3).each {
+        echo "Number: " + it
+    }
+
+    currentBuild.result = 'SUCCESS' //FAILURE to fail
+    return this
 }
