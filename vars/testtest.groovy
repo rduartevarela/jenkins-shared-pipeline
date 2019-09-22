f28b1e541f3d10c7d873c710be60cee75b2f5d3a
Ricardo Varela <rvarela@deloitte.pt>
2019-09-22 18:53:55 +0100

    Add first method

--- vars/testtest.groovy
+++ vars/testtest.groovy
@@ -1,11 +1,33 @@
 #!/usr/bin/env groovy
 
 def call(body) {
-    stage('Test') {
-		steps {
-		
-			echo "Check status"
+
+	pipeline {
+		agent any	
+
+		stages {
+			stage('Build') {
+				steps {
+					echo 'Building..'
+					bat 'mvn install -f formacaoJava2019/pom.xml'
+				}
+			}
 			
+			stage('Test') {
+				steps {
+				
+					echo "Check status"
+					
+				}
+			}
+			
+			stage('Deploy') {
+				steps {
+					echo 'Deploying....'
+					bat "xcopy \"formacaoJava2019\\target\\formacaoJava2019-0.0.1-SNAPSHOT.war\" \"C:\\Tomcat\\apache-tomcat-8.5.46\\webapps\" /y"
+				}
+			}
 		}
 	}
+
 }
