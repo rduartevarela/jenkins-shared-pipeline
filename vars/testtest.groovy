e6a0664f0840ad27ec34a92ab1d73701f5bf54bd
Ricardo Varela <rvarela@deloitte.pt>
2019-09-22 18:48:29 +0100

    Add first method

--- vars/testtest.groovy
+++ vars/testtest.groovy
@@ -1,12 +1,9 @@
 #!/usr/bin/env groovy
 
-def call(body) {
-    echo "Check status"
-
-    (1..3).each {
-        echo "Number: " + it
-    }
-
-    currentBuild.result = 'SUCCESS' //FAILURE to fail
-    return this
+stage('Test') {
+	steps {
+	
+		echo "Check status"
+		
+	}
 }
