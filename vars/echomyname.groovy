fc525cb44661eb9ec0080a474985a024f2ed2e85
Ricardo Varela <rvarela@deloitte.pt>
2019-09-23 13:06:05 +0100

    Add method to shared variables

--- vars/echomyname.groovy
+++ vars/echomyname.groovy
@@ -1,6 +1,6 @@
 def call(Map config) {
 
-	if(config.curretnbuild%2 == 0){
+	if(config.currentbuild%2 == 0){
 		echo "Hello World: ${config.name} "
 	} else {
 		echo "Hello World: ${config.surname}"
